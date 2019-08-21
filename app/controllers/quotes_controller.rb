7b3f4423c7b9eb26ff663b7f9e3bbf018283addf
Robyn Wang <tyrobynwang@gmail.com>
2019-08-21 00:14:29 +0000

    splurty 13

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -10,12 +10,11 @@ class QuotesController < ApplicationController
     def create
         Quote.create(quote_params)
         redirect_to root_path
-    end
-
-private
-
-def quote_params
-    params.require(:quote).permit(:saying, :author)
   end
+
+    private
+
+    def quote_params
+        params.require(:quote).permit(:saying, :author)
 end
 
